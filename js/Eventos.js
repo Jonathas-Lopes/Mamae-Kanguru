f2be5c3538f2e091f1fd27eb03d6ed2ba44af43d
Jonathas <jonathas.Lopes@silicon-meadow.com.br>
2020-06-21 12:35:37 -0300

    Adequando projeto para o padrao laravel, estruturando migrations and models

--- js/Eventos.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    
-    /** código de inicialização do dropdown */
-  $('.dropdown-trigger').dropdown();
-
-
-
-});
--- js/Solidariedade.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    
-    $('.botaoanuncio').click(function () {
-        $('.modal').modal();
-    });
-
-
-
-});
