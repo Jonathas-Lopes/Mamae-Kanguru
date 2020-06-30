64353282337a0839095d343a136b6f356ff219ca
paulanguedes <paulanguedes@gmail.com>
2020-06-30 11:56:05 -0300

    VIEWS / RESERVAS: crianção do Manual de Boas Práticas padrão de todos os espaços

--- resources/views/espacosReservas.blade.php
+++ resources/views/espacosReservas.blade.php
@@ -38,17 +38,30 @@
             <p class="card-content">Por favor, antes de reservar, fique ciente e certifique-se de que você poderá cumprir as Regras de Utilização durante todo o período da sua reserva.</p>
             <ol class="card-content">
                 <li>Os espaços só poderão ser reservados pelos moradores, que se responsabilizarão pelo bom uso, manutenção do local e eventuais danos a propriedade.</li>
+                <li>Ao retirar e entregar as chaves do espaço na portaria, o morador responsável da unidade deve assinar o livro de reservas na portaria.</li>
                 <li>Enviar lista de convidados com nomes completos até 24 horas antes do evento. Informe aos seus convidados que eles deverão apresentar o documento de  identidade na portaria antes de ingressar no condomínio.</li>
                 <li>O  mesmo vale para prestadores de serviço que trabalharão na montagem/desmontagem ou durante o evento: uma lista deve ser enviada com os nomes completos e os mesmos deverão se identificar na portaria com um documento de  identidade.</li>
                 <li>Durante o evento, os convidados não podem  utilizar outros espaços de lazer do condomínio que não façam  parte da área locada, como quadra, sauna, academia, entre outros.</li>
-                <li>De acordo com as  normas vigentes do condomínio e lei municipal, não é permitida qualquer atividade que produza barulho excessivo ou que perturbe o sossego dos moradores antes das 8h00 e após às 22h00 (ou 23h55 aos sábados). Sendo assim, é vetado o uso de aparelhos de som fora do horário permitido.</li>
+                <li>De acordo com as  normas vigentes do condomínio e lei municipal, não é permitida qualquer atividade que produza barulho excessivo ou que perturbe o sossego dos moradores antes das 8h00 e após às 22h00 (ou 23h00 aos sábados). Sendo assim, é vetado o uso de aparelhos de som fora do horário permitido.</li>
                 <li>Em caso de perturbação do silêncio e da ordem, o evento deve ser encerrado imediatamente e as devidas providências serão tomadas com base em votação extraordinária em assembléia.</li>
+                <li>Caso haja eventos agendados no dia seguinte, ao final do evento, o local deve ser entregue limpo e o lixo deve ser retirado das lixeiras do espaço. Se informe com  a portaria sobre a agenda.</li>
             </ol> 
         </div>
         <div class="card white col l3 center-align">
-            {{-- Tag PADRÃO  DE TODOS OS ESPAÇOS - Manual de boas prática --}}
-            <h5 class="card-title">Manual de boas práticas</h5>
-            <p class="card-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis beatae vitae sapiente totam nobis, inventore at quaerat vero eveniet quam delectus hic fugiat veritatis quibusdam aut numquam ipsum tempora labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aspernatur voluptatum consequatur reiciendis debitis, ab sint consectetur nam harum dolore est excepturi optio quia temporibus provident iste enim ut? Quam!</p>
+            {{-- Tag PADRÃO  DE TODOS OS ESPAÇOS - Manual de Boas Práticas --}}
+            <h5 class="card-title">Manual de Boas Práticas</h5>
+            <p class="card-content">Esse Manual foi elaborado para garantir que o seu evento seja bem sucedido e ocorra da melhor maneira possível. Essas regras não são obrigatória, mas somente uma sugestão.</p>
+            <p class="card-content">Mas, lembramos que o morador da unidade que reservou o espaço será responsabilizado por eventuais ocorrências de qualquer natureza.</p>
+            <p class="card-content">Sendo assim, as boas práticas recomendadas são:</p>
+            <ul class="card-content">
+                <li>Se ao entrar no espaço pela primeira vez, antes do seu evento, e notar algum dano ao espaço ou aos itens que fazem parte do mesmo, notifique imediatamente o porteiro para evitar problemas futuros.</li>
+                <li>Para eventos com  mais de 70 pessoas, recomendamos a contratação de um agente de segurança particular, que assegurará que as Regras de Utilização sejam cumpridas por todos os convidados, bem como zelará pela segurança em geral. Lembramos também que cada espaço possui uma capacidade  máxima de lotação. Por favor, respeite-a para evitar problemas graves durante o seu evento.</li>
+                <li>Para evitar acidentes, recomendamos que evite usar utensílios de materiais que, ao quebrarem, sejam cortantes, como o vidro.</li>
+                <li>Também recomendamos que usem bebidas em latas ou garrafas de plástico ao invés de vidro, pelo motivo exposto anteriormente.</li>
+                <li>Depois das 22 horas (23 aos sábados), peça para os seus convidados para permanecerem nas áreas fechadas do espaço. Assim não incomodará os vizinhos que querem descansar e evitará situações desconfortáveis ou até a interrupção do seu evento.</li>
+                <li>Não incentive seus convidados a trazerem os seus bichinhos de estimação para o evento. Tendo em vista que eles podem se assustar com a quantidade de pessoas, ter atitudes agressivas pelo excesso de barulho ou danificar os móveis do salão.</li>
+            </ul>
+            
     </section>
 
     
