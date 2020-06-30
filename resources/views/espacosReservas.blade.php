4fc06485cfe613dbdc272a3c3adcc10d2d9d455b
paulanguedes <paulanguedes@gmail.com>
2020-06-30 10:22:00 -0300

    Criação do template padrão de cada espaçode lazer para locação

--- resources/views/espacosReservas.blade.php
+++ resources/views/espacosReservas.blade.php
@@ -8,10 +8,13 @@
             <img class="imagemespaco" width="100%" src="../UNIU/img/condominio_espaco_gourmet.jpg" alt="Foto Espaço Gourmet">
         </div>
         <div class="detalhelocacao space-title col l7 m12 s12">
-            <h3 class="space-title center-align">Espaço Gourmet</h3>
+            {{-- Tag de título do espaço --}}
+            <h3 class="space-title center-align">Título do Espaço</h3>
+            {{-- Date picker/Agenda do espaço --}}
             <p class="content center-align">selecione uma data</p>
             <div class="selecaodata">
             <input type="text" class="datepicker center">
+            {{-- Botão de reservar --}}
             <a class="waves-effect waves-teal btn-flat amber accent-4 black-text center">reservar</a>
             </div>
         </div>
@@ -20,17 +23,21 @@
     <section class="detalhesespaco section">
         <div class="card white col l2 center-align">
             <h5 class="card-title">Sobre o espaço</h5>
+            {{-- Tag para BREVE descrição e localização do espaço --}}
             <p class="card-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis beatae vitae sapiente totam nobis, inventore at quaerat vero eveniet quam delectus hic fugiat veritatis quibusdam aut numquam ipsum tempora labore.</p>
         </div>
         <div class="card white col l2 center-align">
             <h5 class="card-title">O espaço inclui:</h5>
+            {{-- Tag inclusão de itens que o espaço inclui --}}
             <p class="card-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis beatae vitae sapiente totam nobis, inventore at quaerat vero eveniet quam delectus hic fugiat veritatis quibusdam aut numquam ipsum tempora labore.</p>
         </div>
         <div class="card white col l3 center-align">
+            {{-- Tag PADRÃO  DE TODOS OS ESPAÇOS - Regras de utilização --}}
             <h5 class="card-title">Regras de utilização</h5>
             <p class="card-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis beatae vitae sapiente totam nobis, inventore at quaerat vero eveniet quam delectus hic fugiat veritatis quibusdam aut numquam ipsum tempora labore. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid nostrum ad qui, beatae sequi deleniti omnis sapiente possimus natus illum eos accusamus enim libero totam eum dolores culpa minima aperiam!</p>
         </div>
         <div class="card white col l3 center-align">
+            {{-- Tag PADRÃO  DE TODOS OS ESPAÇOS - Manual de boas prática --}}
             <h5 class="card-title">Manual de boas práticas</h5>
             <p class="card-content">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis beatae vitae sapiente totam nobis, inventore at quaerat vero eveniet quam delectus hic fugiat veritatis quibusdam aut numquam ipsum tempora labore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae aspernatur voluptatum consequatur reiciendis debitis, ab sint consectetur nam harum dolore est excepturi optio quia temporibus provident iste enim ut? Quam!</p>
     </section>
