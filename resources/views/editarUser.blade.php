142c05ceb5b07953b964e0383627a2f35de2c20c
Paula N Guedes <paulanguedes@gmail.com>
2020-07-09 11:02:14 -0300

    Criação da view para edição usuário logado

---
+++ resources/views/editarUser.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.template')
+
+@section('titulo') Editar usuário @endsection
+
+@section('conteudo')
+
+    <link type="text/css" rel="stylesheet" href="./css/editarUser.css"/>
+
+    <main id="editarUser">
+
+    </main>
+
+@endsection
