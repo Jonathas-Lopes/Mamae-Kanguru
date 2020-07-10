835a6cbd05b55556b33b68f61c1aa91ae6bc65e4
Paula N Guedes <paulanguedes@gmail.com>
2020-07-10 10:50:00 -0300

    EVENTOS: carousel com próximos eventos

--- resources/views/Eventos.blade.php
+++ resources/views/Eventos.blade.php
@@ -5,41 +5,101 @@
 @endsection
 
 @section('conteudo')
-    <section id="eventos">
+    <main id="eventos">
+
         <link type="text/css" rel="stylesheet" href="./css/eventos.css" />
 
-            <div class="eventodestaque container col s12 m7">
-                <h2 class="header">Próximos eventos</h2>
-                <div class="card horizontal">
-                    <div class="card-image">
-                        <img src="./img/festa-junina.jpg" alt="Foto Festa Junina">
-                    </div>
-                    <div class="card-stacked">
-                        <div class="card-content">
-                            <span class="card-title">Festa Junina 2020</span>
-                            <h6>28/06</h5>
-                            <h6>A partir das 11h00</h6>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, commodi tenetur. Corporis accusamus ratione eligendi ipsa aliquam ut, voluptas, deleniti dignissimos veritatis libero laborum aspernatur necessitatibus minima. Sapiente, ipsum dolorum.</p>
-                        </div>
-                    </div>
-                </div>
+        <div class="carousel carousel-slider center" data-indicators="true">
+
+            <div class="carousel-item indigo darken-4 white-text">
+                {{-- Título evento --}}
+                <h4 class="white-text">Churrasco de Dia dos Pais</h4>
+                {{-- Data do evento --}}
+                <h5>08/08/2020 - sábado</h5>
+                {{-- Horário do evento --}}
+                <h5>A partir das 11h00</h5>
+                {{-- Descrição do evento --}}
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                {{-- Botão do usuário --}}
+                <a class="confirmarbtn waves-effect waves-light btn">confirmar presença</a>
+                {{-- Botões do administrador --}}
+                <a class="editarbtn btn-floating btn-large waves-effect"><i class="material-icons">edit</i></a>
+                <a class="deletarbtn btn-floating btn-large waves-effect red"><i class="material-icons">remove</i></a>
             </div>
 
-            <div class="galeriaeventos center container col s12 m7">
-                <h2 class="header">Galeria de fotos dos eventos</h2>
-                <!-- Dropdown Trigger -->
-                <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Escolha o evento</a>
-
-                <!-- Dropdown Structure -->
-                <ul id='dropdown1' class='dropdown-content'>
-                <li><a href="#!">Festa de Volta às Aulas 2020</a></li>
-                <li><a href="#!">Festa de Natal 2019</a></li>
-                <li><a href="#!">Halloween 2019</a></li>
-                </ul>
+            <div class="carousel-item green darken-4 white-text">
+                {{-- Título evento --}}
+                <h4 class="white-text">Dias das Crianças</h4>
+                {{-- Data do evento --}}
+                <h5>11/10/2020 - domingo</h5>
+                {{-- Horário do evento --}}
+                <h5>A partir das 10h00</h5>
+                {{-- Descrição do evento --}}
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                {{-- Botão do usuário --}}
+                <a class="confirmarbtn waves-effect waves-light btn">confirmar presença</a>
+                {{-- Botões do administrador --}}
+                <a class="editarbtn btn-floating btn-large waves-effect"><i class="material-icons">edit</i></a>
+                <a class="deletarbtn btn-floating btn-large waves-effect red"><i class="material-icons">remove</i></a>
+            </div>
+            
+            <div class="carousel-item orange darken-4 white-text">
+                {{-- Título evento --}}
+                <h4 class="white-text">Dias das Bruxas</h4>
+                {{-- Data do evento --}}
+                <h5>31/10/2020 - sábado</h5>
+                {{-- Horário do evento --}}
+                <h5>A partir das 18h00</h5>
+                {{-- Descrição do evento --}}
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                {{-- Botão do usuário --}}
+                <a class="confirmarbtn waves-effect waves-light btn">confirmar presença</a>
+                {{-- Botões do administrador --}}
+                <a class="editarbtn btn-floating btn-large waves-effect"><i class="material-icons">edit</i></a>
+                <a class="deletarbtn btn-floating btn-large waves-effect red"><i class="material-icons">remove</i></a>
             </div>
 
+            <div class="carousel-item red darken-4 white-text">
+                {{-- Título evento --}}
+                <h4 class="white-text">Sorteio do Salão (Natal e Ano Novo)</h4>
+                {{-- Data do evento --}}
+                <h5>01/11/2020 - domingo</h5>
+                {{-- Horário do evento --}}
+                <h5>A partir das 20h00</h5>
+                {{-- Descrição do evento --}}
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                <p class="white-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate asperiores exercitationem nihil facilis! Assumenda suscipit veritatis aspernatur distinctio, voluptatem ullam molestiae tempora voluptatum dignissimos consequatur debitis rerum asperiores consequuntur possimus!</p>
+                {{-- Botão do usuário --}}
+                <a class="confirmarbtn waves-effect waves-light btn">confirmar presença</a>
+                {{-- Botões do administrador --}}
+                <a class="editarbtn btn-floating btn-large waves-effect"><i class="material-icons">edit</i></a>
+                <a class="deletarbtn btn-floating btn-large waves-effect red"><i class="material-icons">remove</i></a>
+            </div>
+        </div>
 
-        </section>
+
+        <div class="galeriaeventos center container col s12 m7">
+            <h4 class="header">Galeria de fotos dos eventos</h4>
+            <!-- Dropdown Trigger -->
+            <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Escolha o evento</a>
+
+            <!-- Dropdown Structure -->
+            <ul id='dropdown1' class='dropdown-content'>
+            <li><a href="#!">Festa de Volta às Aulas 2020</a></li>
+            <li><a href="#!">Festa de Natal 2019</a></li>
+            <li><a href="#!">Halloween 2019</a></li>
+            </ul>
+        </div>
+
+
+        </main>
         <script src="./js/jQuery341.js"></script>
         <script src="./js/materialize.min.js"></script>
         <script src="./js/Eventos.js"></script>
