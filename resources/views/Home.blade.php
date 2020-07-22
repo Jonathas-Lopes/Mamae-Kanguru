8032a33ff7a294c4f85704d4ff150d269fa81d63
Paula N Guedes <paulanguedes@gmail.com>
2020-07-21 22:40:10 -0300

    alteações views admin

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -13,10 +13,12 @@
                 <img class="circle" src="./img/foto_morador.jpg" alt="Foto perfil">
             @endif
 
+            @if(Auth::user()->admin == 0)
             <!-- Modal Trigger -->
             <a class="btn-floating btn-large waves-effect modal-trigger" data-target="modal1" href="#modal1">
                 <i class="material-icons">edit</i>
             </a>
+            @endif
         </div>
 
         <!-- Modal Structure -->
@@ -97,17 +99,20 @@
                 <a class="linksperfil waves-effect waves btn-flat">Mensagens privadas</a>
                 <a class="linksperfil waves-effect waves btn-flat">Configurações</a>
             </div>
-            <div class="blocohashtags center-align">
-                <a class="hashtags waves-effect waves btn-flat">#procuro</a>
-                <a class="hashtags waves-effect waves btn-flat">#ofereço</a>
-                <a class="hashtags waves-effect waves btn-flat">#aviso</a>
-            </div>
+            @if (Auth::user()->admin == 0)
+                <div class="blocohashtags center-align">
+                    <a class="hashtags waves-effect waves btn-flat">#procuro</a>
+                    <a class="hashtags waves-effect waves btn-flat">#ofereço</a>
+                    <a class="hashtags waves-effect waves btn-flat">#aviso</a>
+                </div>   
+            @endif
         </div>
     </section>
 
-    {{-- Mensagens instantâneas --}}
     <section class="assuntos">
 
+        {{-- Mensagens instantâneas --}}
+        @if (Auth::user()->admin == 0)
         <article class="card">
             {{-- textarea --}}
             <div class="textarea">
@@ -142,39 +147,74 @@
                 <h6><img class="circle" src="./img/foto_moradora2.jpg" alt="Foto perfil">Valéria Costa</h6>
                 <p>No último sábado desse mês vou levar doações para a casa de repouso. Quem tiver doações para entregar, pode aproveitar que tem espaço no carro.</p>
                 <h5>#ofereço</h5>
-                <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @if (Auth::user()->admin == 0)
+                    <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @endif
+                @if (Auth::user()->admin == 1)
+                    <a class="botaoresponder btn waves-effect waves-light"><i class="material-icons">delete</i></a>
+                @endif
             </article>
             <article class="card">
                 <h6><img class="circle" src="./img/foto_morador.jpg" alt="Foto perfil">Francisco Matos</h6>
                 <p>Alguém possui indicação de diarista?</p>
                 <h5>#procuro</h5>
-                <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @if (Auth::user()->admin == 0)
+                    <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @endif
+                @if (Auth::user()->admin == 1)
+                    <a class="botaoresponder btn waves-effect waves-light"><i class="material-icons">delete</i></a>
+                @endif
             </article><article class="card">
                 <h6><img class="circle" src="./img/foto_morador1.jpg" alt="Foto perfil">Bruno Galvão</h6>
                 <p>Todos os dias levo meu filho ao colégio São João às 7:10. Sei que tem outras crianças que estudam lá. Se alguém quiser carona, tenho mais três vagas no carro.</p>
                 <h5>#ofereço</h5>
-                <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @if (Auth::user()->admin == 0)
+                    <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @endif
+                @if (Auth::user()->admin == 1)
+                    <a class="botaoresponder btn waves-effect waves-light"><i class="material-icons">delete</i></a>
+                @endif
             </article><article class="card">
                 <h6><img class="circle" src="./img/foto_moradora.jpg" alt="Foto perfil">Helena Cardin</h6>
                 <p>Olá! Estou doando um jogo de taças de vinho. São de vidro e está faltando uma. São 5 no total.</p>
                 <h5>#ofereço</h5>
-                <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @if (Auth::user()->admin == 0)
+                    <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @endif
+                @if (Auth::user()->admin == 1)
+                    <a class="botaoresponder btn waves-effect waves-light"><i class="material-icons">delete</i></a>
+                @endif
             </article><article class="card">
                 <h6><img class="circle" src="./img/foto_morador2.jpg" alt="Foto perfil">Carlos Ruiz</h6>
                 <p>Alguém tem vaga de garagem disponível para alugar?</p>
                 <h5>#procuro</h5>
-                <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @if (Auth::user()->admin == 0)
+                    <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @endif
+                @if (Auth::user()->admin == 1)
+                    <a class="botaoresponder btn waves-effect waves-light"><i class="material-icons">delete</i></a>
+                @endif
             </article><article class="card">
                 <h6><img class="circle" src="./img/foto_moradora1.JPG" alt="Foto perfil">Sandra Maria Alves</h6>
                 <p>Quem aqui vai na aula de zumba no SESC toda terça e quinta às 10:30 da manhã? Eu e a Clotilde do 71B vamos e quem quiser, pode ir com a gente.</p>
                 <h5>#ofereço</h5>
-                <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @if (Auth::user()->admin == 0)
+                    <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @endif
+                @if (Auth::user()->admin == 1)
+                    <a class="botaoresponder btn waves-effect waves-light"><i class="material-icons">delete</i></a>
+                @endif
             </article>
             </article><article class="card">
                 <h6><img class="circle" src="./img/foto_morador3.jpg" alt="Foto perfil">Pedro Trindade</h6>
                 <p>Estou precisando dedetizar o apê. Alguém tem indicação de profissional ou empresa que faz esse serviço?</p>
                 <h5>#procuro</h5>
-                <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @if (Auth::user()->admin == 0)
+                    <a class="botaoresponder btn-floating btn waves-effect waves-light"><i class="material-icons">reply</i></a>
+                @endif
+                @if (Auth::user()->admin == 1)
+                    <a class="botaoresponder btn waves-effect waves-light"><i class="material-icons">delete</i></a>
+                @endif
             </article>
         
         </div>
