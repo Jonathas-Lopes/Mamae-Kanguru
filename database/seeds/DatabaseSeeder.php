8f413bd262dcc33680562c1d9020ed9410c8cb2c
Paula N Guedes <paulanguedes@gmail.com>
2020-07-23 09:57:52 -0300

    arrumando DatabaseSeeder e seeder de MENSAGENS

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,11 @@ class DatabaseSeeder extends Seeder
         $this->call(CondominioSeeder::class);
         $this->call(UsuarioSeeder::class);
         $this->call(AreaSeeder::class);
-        // $this->call(AvisosSeeder::class);
+        $this->call(AvisosSeeder::class);
         $this->call(EventoSeeder::class);
+        $this->call(MensagensSeeder::class);
+        $this->call(PainelSeeder::class);
+        $this->call(SolidariedadeSeeder::class);
+
     }
 }
