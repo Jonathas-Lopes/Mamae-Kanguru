3514745e5614bf50d8538dceff91c927fb9327e0
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-07-26 19:02:14 -0300

    subindo últimas alterações

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,10 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('APP_ENV') !== 'local')
-        { 
-            $url->forceSchema('https');
-        }
-
+        
     }
 }
