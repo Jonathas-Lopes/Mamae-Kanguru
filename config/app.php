c9b488aaaf73dda49f46a160df8ba3a632d36cbc
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-07-26 16:24:44 -0300

    alterando url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'APP_URL=http://uniu-evapericorashid.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
